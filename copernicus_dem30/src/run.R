7c144abfe5536afe0e63477bb1eee1a13380fbff
AlexDo1 <alexander.dolich@kit.edu>
2024-07-04 10:07:55 +0200

    run.R to run tool

---
+++ copernicus_dem30/src/run.R
@@ -0,0 +1,36 @@
+# load json2aRgs for parameter parsing
+library(json2aRgs)
+
+# load the extract_corine function
+source("extract_copernicus_dem30.R")
+
+# get the parameters for the tool
+params <- get_parameters()
+
+# # get the data paths for the tool
+data <- get_data(return_data_paths = TRUE)
+
+# check if a toolname was set in env
+toolname <- tolower(Sys.getenv("TOOL_RUN"))
+
+# if no toolname was set, stop the script
+if (toolname == "") {
+  stop("No toolname was set in the environment. Please set the TOOL_RUN environment variable.")
+
+} else if (toolname == "topographic_attributes_copernicus_dem30") {
+  # run Python script to download the Copernicus DEM data if it does not exist yet
+  if (!file.exists("/in/dem/dem_merged.tif")) {
+    system("python3 /src/download_copernicus_dem30.py")
+  } else {
+    print("The Copernicus DEM data already exists, delete in/dem_merged.tif if your catchments changed and rerun the tool.")
+  }
+  
+  # extract the Copernicus DEM data
+  extract_dem_data(catchments = data$catchments,
+                   stations = data$stations,
+                   id_field_name_catchments = params$id_field_name_catchments,
+                   id_field_name_stations = params$id_field_name_stations)
+
+} else {
+  stop("The toolname '", toolname, "' is not supported.")
+}
